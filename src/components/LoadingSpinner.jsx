8fb1917854f4e8439ba52e27b9b64d6b9453bf79
mdsadatrahman03 <mdsadatrahman03@gmail.com>
2026-05-19 15:09:42 +0600

    Create ProtectedRoute and LoadingSpinner components

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center min-h-[60vh]">
+    <div className="w-12 h-12 border-4 border-indigo-600 border-t-transparent rounded-full animate-spin" />
+  </div>
+);
+
+export default LoadingSpinner;
