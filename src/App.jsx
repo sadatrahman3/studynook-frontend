12634ed4ab99d63b48f7d756e03070396fb7311d
mdsadatrahman03 <mdsadatrahman03@gmail.com>
2026-05-19 15:09:39 +0600

    Set up App entry point with routing and providers

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+import PublicLayout from './layouts/PublicLayout';
+import PrivateLayout from './layouts/PrivateLayout';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Rooms from './pages/Rooms';
+import RoomDetails from './pages/RoomDetails';
+import AddRoom from './pages/AddRoom';
+import MyListings from './pages/MyListings';
+import MyBookings from './pages/MyBookings';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <Toaster position="top-right" toastOptions={{
+            className: 'dark:bg-gray-800 dark:text-white',
+          }} />
+          <Routes>
+            <Route element={<PublicLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/rooms" element={<Rooms />} />
+              <Route path="/rooms/:id" element={<RoomDetails />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+            <Route element={<PrivateLayout />}>
+              <Route path="/add-room" element={<ProtectedRoute><AddRoom /></ProtectedRoute>} />
+              <Route path="/my-listings" element={<ProtectedRoute><MyListings /></ProtectedRoute>} />
+              <Route path="/my-bookings" element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
